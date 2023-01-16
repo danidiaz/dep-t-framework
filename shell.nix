b0fbde361680b9416cc88db98d1af6d3359e86cf
Daniel Díaz <diaz_carrete@yahoo.com>
2023-01-16 21:15:39 +0100

    New dep-t version.

--- shell.nix
+++ shell.nix
@@ -1,11 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     packages =
-        let haskellPackages = pkgs.haskell.packages.ghc924 ;
-         in
          [
-            (haskellPackages.ghcWithPackages (hpkgs : [hpkgs.lens hpkgs.servant hpkgs.servant-server hpkgs.warp]))
-            haskellPackages.cabal-install
-            haskellPackages.haskell-language-server
+            (pkgs.haskellPackages.ghcWithPackages (hpkgs : [
+                hpkgs.wai-app-static
+                ]))
+            pkgs.cabal-install
+            pkgs.haskell-language-server
+            pkgs.watchexec
          ];
 }
